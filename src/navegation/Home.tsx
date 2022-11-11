a8970674777780fd0fb468202d7e06997f86a290
Manuel <manuelruiz26042000@gmail.com>
2022-11-11 17:52:05 -0600

    creacion de componente navbar

--- src/navegation/Home.tsx
+++ src/navegation/Home.tsx
@@ -1,11 +1,50 @@
 import { View, Text, StyleSheet } from "react-native"
 
+import React from 'react';
+import { Image, ScrollView,TouchableOpacity} from 'react-native';
+
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 500,
+  height: 64,
+};
+
 export function Home (){
     return(
         <View style={styles.container} >
-            <Text>
-                Setting  sacreem
-            </Text>
+            <ScrollView>
+            <Text style={{ fontSize: 50 }}>Scroll me plz</Text>
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Text style={{ fontSize: 50 }}>If you like</Text>
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Text style={{ fontSize: 50 }}>Scrolling down</Text>
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Text style={{ fontSize: 50 }}>What's the best</Text>
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Text style={{ fontSize: 50 }}>Framework around?</Text>
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Image source={logo} />
+            <Text style={{ fontSize: 50 }}>React Native</Text>
+        </ScrollView>
         </View>
 
     );
